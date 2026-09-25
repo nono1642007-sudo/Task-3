5197534fed2d62a97ecc5106cb219ba131d5c75a
Nour <nono1642007@gmail.com>
2026-09-26 00:08:17 +0300

    completed

--- student_performance.py
+++ student_performance.py
@@ -41,6 +41,24 @@ df["Passed"]=pd.to_numeric(
 )
 df=df[df["Passed"].isin([0,1])].copy()
 df["Passed"]=df["Passed"].astype(int)
+
+if "Study Hours per Week" in df.columns:
+    df.loc[df["Study Hours per Week"] < 0, "Study Hours per Week"] = pd.NA
+
+if "Attendance Rate" in df.columns:
+    df.loc[
+        (df["Attendance Rate"] < 0) | (df["Attendance Rate"] > 100),
+        "Attendance Rate"
+    ] = pd.NA
+
+if "Previous Grade" in df.columns:
+    df.loc[
+        (df["Previous Grade"] < 0) | (df["Previous Grade"] > 100),
+        "Previous Grade"
+    ] = pd.NA
+
+print("Invalid values corrected.")
+
 print("Passed converted to binary classification.")
 print("Passed values:")
 print(df["Passed"].value_counts())
@@ -71,25 +89,30 @@ for column in categorical_columns:
 print("Missing values handled successfully.")
 
 if "Parent Education Level" in df.columns:
-    education_mapping={
-        "high school":0,
-        "associate":1,
-        "baccalaureate":2,
-        "bachelor":2,
-        "master":3,
-        "doctorate":4
+    education_mapping = {
+        "high school": 0,
+        "associate": 1,
+        "baccalaureate": 2,
+        "bachelor": 2,
+        "master": 3,
+        "doctorate": 4
     }
 
-education=(
-    df["Parent Education Level"] .astype(str) .str.strip().str.lower()
+    education = (
+        df["Parent Education Level"]
+        .astype(str)
+        .str.strip()
+        .str.lower()
     )
-df["Parent Education Level"]=education.map(education_mapping)
 
-df["Parent Education Level"]=(
-    df["Parent Education Level"].fillna(df["Parent Education Level"].median())
-)
+    df["Parent Education Level"] = education.map(education_mapping)
 
-print("Parent Education Level encoded successfully.")
+    df["Parent Education Level"] = (
+        df["Parent Education Level"]
+        .fillna(df["Parent Education Level"].median())
+    )
+
+    print("Parent Education Level encoded successfully.")
 
 
 categorical_columns = df.select_dtypes(
@@ -99,12 +122,20 @@ categorical_columns = df.select_dtypes(
 for column in categorical_columns:
     df[column],_ = pd.factorize(df[column])
 print("Categorical data encoded successfully.")
-feature_columns = [
-    column for column in df.columns
-    if column != "Passed"
+
+continuous_columns = [
+    column for column in [
+        "Study Hours per Week",
+        "Attendance Rate",
+        "Previous Grade"
+    ]
+    if column in df.columns
 ]
+
 scaler = StandardScaler()
-df[feature_columns]=scaler.fit_transform(df[feature_columns])
+df[continuous_columns] = scaler.fit_transform(df[continuous_columns])
+
+
 print(" Feature standardization completed successfully.")
 
 print("Remaining missing values:")
